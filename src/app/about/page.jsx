c4fb8b8fbba0612f77c2187d83cb3e9ecb7fac1a
zainab <zizihaji79@gmIL.COM>
2025-02-25 05:33:26 +0330

    add about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Aboutpage() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+        <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+        <p>Welcome to IMDB, your go-to destination for [brief description of what you offer or focus on, e.g., tech reviews, lifestyle tips, etc.]. Founded in [Year], our mission is to provide you with insightful and engaging content that enriches your daily life.</p>
+        <p>We are a passionate team of [industry professionals, writers, or enthusiasts] dedicated to exploring the latest trends and sharing our knowledge with you. Our diverse backgrounds allow us to cover a wide range of topics, ensuring there’s something for everyone.</p>
+        <p>At IMDB, we believe in the power of community and strive to foster a space where people can connect, learn, and grow. Thank you for joining us on this journey—let’s explore the world together!</p>
+    </div>
+  )
+}
+
+export default Aboutpage
